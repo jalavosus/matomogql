4881ddbe5e70d9cc70d021705c4adda6da91729c
Jamie Alavosus <alavosus.james@gmail.com>
2025-04-20 17:18:05 -0400

    gql/gql: cleanup

--- utils/gql/gql.go
+++ utils/gql/gql.go
@@ -2,9 +2,15 @@ package gql
 
 import (
 	"net/http"
+	"time"
 
+	"github.com/99designs/gqlgen/graphql"
 	"github.com/99designs/gqlgen/graphql/handler"
+	"github.com/99designs/gqlgen/graphql/handler/extension"
+	"github.com/99designs/gqlgen/graphql/handler/lru"
+	"github.com/99designs/gqlgen/graphql/handler/transport"
 	"github.com/99designs/gqlgen/graphql/playground"
+	"github.com/vektah/gqlparser/v2/ast"
 
 	"github.com/jalavosus/matomogql/graph"
 	"github.com/jalavosus/matomogql/graph/loaders"
@@ -13,10 +19,9 @@ import (
 
 func MakeServer(enablePlayground bool) http.Handler {
 	matomoClient := matomo.NewClient(matomo.GetEnv())
-
 	execSchema := graph.NewExecutableSchema(graph.Config{Resolvers: graph.NewResolver(matomoClient)})
 
-	var srv = loaders.Middleware(handler.NewDefaultServer(execSchema), matomoClient)
+	var srv = loaders.Middleware(newServer(execSchema), matomoClient)
 
 	mux := http.NewServeMux()
 	mux.Handle("/query", srv)
@@ -25,4 +30,31 @@ func MakeServer(enablePlayground bool) http.Handler {
 	}
 
 	return mux
+}
+
+var transports = []graphql.Transport{
+	transport.Options{},
+	transport.GET{},
+	transport.POST{},
+	transport.MultipartForm{},
+	transport.Websocket{
+		KeepAlivePingInterval: 10 * time.Second,
+	},
+}
+
+func newServer(schema graphql.ExecutableSchema) http.Handler {
+	srv := handler.New(schema)
+
+	for _, t := range transports {
+		srv.AddTransport(t)
+	}
+
+	srv.SetQueryCache(lru.New[*ast.QueryDocument](1000))
+
+	srv.Use(extension.Introspection{})
+	srv.Use(extension.AutomaticPersistedQuery{
+		Cache: lru.New[string](100),
+	})
+
+	return srv
 }
