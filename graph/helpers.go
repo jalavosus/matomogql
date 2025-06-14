68dcffda349da174b0d6f76c98f3af50cafd6abe
Jamie Alavosus <alavosus.james@gmail.com>
2025-06-14 02:37:15 -0400

    graph/helpers: LLMs are actually kinda useful.

--- graph/helpers.go
+++ graph/helpers.go
@@ -6,23 +6,31 @@ import (
 	"github.com/jalavosus/matomogql/graph/model"
 )
 
+// orderLastVisits sorts lastVisits in-place by ServerTimestamp.
+// It defaults to descending order unless a valid ordering option is provided.
 func orderLastVisits(lastVisits []*model.Visit, order *model.OrderByOptions) []*model.Visit {
-	orderBy := model.OrderByDesc
-	if order != nil {
-		if ob, ok := order.Timestamp.ValueOK(); ok {
-			orderBy = *ob
-		}
-	}
+	orderBy := getOrderBy(order)
+	asc := orderBy == model.OrderByAsc
 
 	sort.Slice(lastVisits, func(i, j int) bool {
-		if orderBy == model.OrderByAsc {
+		if asc {
 			return lastVisits[i].ServerTimestamp < lastVisits[j].ServerTimestamp
 		}
-
 		return lastVisits[i].ServerTimestamp > lastVisits[j].ServerTimestamp
 	})
 
 	return lastVisits
+}
+
+// getOrderBy safely extracts the desired ordering, falling back to DESC.
+func getOrderBy(order *model.OrderByOptions) model.OrderBy {
+	if order == nil {
+		return model.OrderByDesc
+	}
+	if ob, ok := order.Timestamp.ValueOK(); ok {
+		return *ob
+	}
+	return model.OrderByDesc
 }
 
 func visitActionByType(visit *model.Visit, eventType string) (ad *model.VisitActionDetails) {
